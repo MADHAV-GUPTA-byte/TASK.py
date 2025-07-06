371b2f32d3b7350b756a31440823150a090d8aa9
MADHAV <madhavgupta123149@gmail.com>
2025-07-06 11:33:30 +0530

    Add files via upload

---
+++ ASSIGNMENT.py
@@ -0,0 +1,9 @@
+#TASK--------> 2
+
+
+
+name_1= input("Enter your first name: ")
+name_2= input("Enter your surname name: ")
+
+name= name_1+name_2
+print("Hello",name+"! Welcome to Python program.")
---
+++ learning.py
@@ -0,0 +1,14 @@
+a=int(input("Enter a number: "))
+b=int(input("Enter 2nd number: "))
+
+print("Addition", a+b)
+print("Subtraction", a-b)
+print("Multiplication", a*b)
+print("Division", a//b)
+
+
+name_1= input("Enter your first name: ")
+name_2= input("Enter your surname name: ")
+
+name= name_1+name_2
+print("Hello",name+"! Welcome to Python program.")
