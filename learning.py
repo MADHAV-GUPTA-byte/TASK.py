3893ee36498929f912c40b9624e15738c0f32b40
MADHAV <madhavgupta123149@gmail.com>
2025-07-06 11:38:08 +0530

    Add files via upload

--- learning.py
+++ learning.py
@@ -7,8 +7,3 @@ print("Multiplication", a*b)
 print("Division", a//b)
 
 
-name_1= input("Enter your first name: ")
-name_2= input("Enter your surname name: ")
-
-name= name_1+name_2
-print("Hello",name+"! Welcome to Python program.")
